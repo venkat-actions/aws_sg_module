d7e1657bd795fb94bac253941bd7047ee5cf1e68
VenkataKoti <venkatakotaiahkanakam@gmail.com>
2026-09-22 12:22:54 +0530

    terraform vpc module development

--- sg.tf
+++ sg.tf
@@ -1,8 +1,14 @@
 resource "aws_security_group" "main" {
-  name        = "${local.common_name}"
+  name        = local.common_name
   description = "Allow traffic for ${var.sg_name} project ${var.project} in ${var.environment}"
   vpc_id      = var.vpc_id
-
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # prefix_list_ids = [aws_vpc_endpoint.my_endpoint.prefix_list_id]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = merge(
     var.sg_tags,
     local.common_tags,
